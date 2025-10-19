b250f85ec61804d21dc5b9d0a5e08d193c8ef1a2
ashutosh2652 <ashutoshkr2652@gmail.com>
2025-10-20 04:38:26 +0530

    signup page and ai integration

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,21 +1,5 @@
-"use client";
-import { useSession, signIn, signOut } from "next-auth/react";
-
-export default function Component() {
-	const { data: session } = useSession();
-	if (session)
-		return (
-			<>
-				Signed In as {session.user.email}
-				<br />
-				<button onClick={() => signOut()}>Sign Out</button>
-			</>
-		);
-	return (
-		<>
-			Not signed In
-			<br />
-			<button onClick={() => signIn()}>Sign In</button>
-		</>
-	);
+function Page() {
+	return <div>Sign-In Page</div>;
 }
+
+export default Page;
