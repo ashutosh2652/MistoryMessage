b8a6a37cad8464d5de7e443cd088bb1bbcf0494c
ashutosh2652 <ashutoshkr2652@gmail.com>
2025-10-20 17:42:52 +0530

    sign-in and sign-up page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 				<body
 					className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 				>
-					<main>{children}</main>
+					{children}
 					<Toaster />
 				</body>
 			</AuthProvider>
---
+++ src/app/u/[username]/page.tsx
@@ -0,0 +1,5 @@
+function Page() {
+	return <div>Message</div>;
+}
+
+export default Page;
